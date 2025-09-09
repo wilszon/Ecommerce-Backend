1e52e6a0fbf45b12a732c6048009be6c09d4372c
Wilson Suarez <wsuarez874@unab.edu.co>
2025-09-09 14:35:11 -0400

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/create', [ProductController::class, 'create']);
-Route::get('/products/{id}/{category?}', [ProductController::class, 'detail']);
 
-
+Route::prefix('products')->controller(ProductController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/create', 'create');
+    Route::get('/{id}/{category?}', 'detail');
+});
