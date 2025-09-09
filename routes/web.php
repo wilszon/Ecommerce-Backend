46f2bf34d4c2db781adec9b53dd2c634df8f5500
Wilson Suarez <wsuarez874@unab.edu.co>
2025-09-09 13:55:57 -0400

    Rutas Basicas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return "List products";
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    if ($category != null){
+        return "Detail products: " . $id . "With Category: " . $category;
+    }
+    else
+    {
+        return "Detail products: " . $id;
+    }
+    
+});
