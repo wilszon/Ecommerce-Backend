f260db23b92ba73d97c2973815402aa77b05fd88
Wilson Suarez <wsuarez874@unab.edu.co>
2025-10-14 14:42:56 -0500

    Se configuro el login en Español y la ruta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,14 +13,16 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    @yield('css')
 
+ 
     
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/products') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,4 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::get('/', [HomeController::class, 'welcome']);
